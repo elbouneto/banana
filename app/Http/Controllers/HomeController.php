00253c5bbe3c00b88c7eb585eff7ba1e6ada7319
elbouneto <elbouneto@gmail.com>
2016-03-17 09:03:56 +0100

    pull17

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
         $actors = new Actors();
 
         $directors = new Directors();
-
+git 
 
         $nb = $movie->getNbMoviesActifs();
         $nbtotal=$movie->getNbMovies();
